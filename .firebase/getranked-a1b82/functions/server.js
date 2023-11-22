c993a75a3bd1f09f6b7b6a0e613503e88ae0260c
Reuben Lee Suppiah (MU) <1201100652@student.mmu.edu.my>
2023-11-22 21:20:36 +0800

    Delete firebase files

--- .firebase/getranked-a1b82/functions/server.js
+++
@@ -1,4 +0,0 @@
-import { onRequest } from 'firebase-functions/v2/https';
-  const server = import('firebase-frameworks');
-  export const ssrgetrankeda1b82 = onRequest({"region":"asia-east1","maxInstance":1}, (req, res) => server.then(it => it.handle(req, res)));
-  
--- .firebase/getranked-a1b82/hosting/_app/immutable/chunks/navigation.c9e7eeab.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./singletons.5718b987.js";const e=o("goto");export{e as g};
