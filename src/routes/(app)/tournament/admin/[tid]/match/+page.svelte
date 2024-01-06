735db949b83c7280c02043adb2daa40192651409
Reuben Lee Suppiah (MU) <1201100652@student.mmu.edu.my>
2024-01-06 22:31:00 +0800

    Next Match

--- src/routes/(app)/tournament/admin/[tid]/match/+page.svelte
+++ src/routes/(app)/tournament/admin/[tid]/match/+page.svelte
@@ -454,6 +454,11 @@
     <div class="flex flex-col">
         {#if drawResponse != undefined}
             {#each drawResponse as category}
+            <button class="btn variant-filled w-14 mt-2" on:click={getDraw}>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99" />
+                </svg>
+            </button>
             <div class="">
                 <br>
                 <span class="text-4xl text-red-300">
@@ -471,6 +476,7 @@
                                 {#each round as match}
                                     <div class="card m-2 variant-filled-surface p-4 w-44 rounded-md"> 
                                         <div class="flex flex-col">
+                                            <span class="text-red-300 font-extrabold">{match.match}</span>
                                             <span class="text-right ">{match.participant1.first_name} <span>({match.p1_score == null?0:match.p1_score})</span></span>
                                             
                                             <hr>
